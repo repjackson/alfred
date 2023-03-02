18b120205ba6640efd70ec9147abd20b5e733604
r2 <daodev@gratigen.one>
2023-03-02 22:12:25 +0000

    savepoint apoolo libs nonworking

---
+++ client/apollo.js
@@ -0,0 +1,14 @@
+// import { setup } from 'meteor/swydo:blaze-apollo';
+
+// const client = new ApolloClient(meteorClientConfig());
+
+// setup({ client });
+
+
+// Template.graph.helpers({
+//   hello() {
+//     return Template.instance().gqlQuery({
+//       query: HELLO_QUERY
+//     }).get();
+//   }
+// });
