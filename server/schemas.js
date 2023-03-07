b531ac784a7ab15fac833818865ae3c52e459f86
r2 <daodev@gratigen.one>
2023-03-07 00:31:28 +0000

    back to working without the schemas

--- server/schemas.js
+++ server/schemas.js
@@ -1,21 +1,19 @@
-import SimpleSchema from "simpl-schema";
-// import { AutoFormPlainTheme } from 'meteor/communitypackages:autoform-plain/static'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'            // optional, default theme
-// import '@fortawesome/fontawesome-free/js/all.js' // optional, is using FA5
-import popper from 'popper.js'
-import { AutoFormThemeBootstrap4 } from 'meteor/communitypackages:autoform-bootstrap4/static'
+// import SimpleSchema from "simpl-schema";
+// // import { AutoFormPlainTheme } from 'meteor/communitypackages:autoform-plain/static'
+// import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.css'            // optional, default theme
+// // import '@fortawesome/fontawesome-free/js/all.js' // optional, is using FA5
+// import popper from 'popper.js'
+// import { AutoFormThemeBootstrap4 } from 'meteor/communitypackages:autoform-bootstrap4/static'
 
-global.Popper = popper                           // fix Popper.js issues
-SimpleSchema.extendOptions(['autoform']);
+// global.Popper = popper                           // fix Popper.js issues
+// SimpleSchema.extendOptions(['autoform']);
 
-AutoFormThemeBootstrap4.load()
+// AutoFormThemeBootstrap4.load()
 
-AutoFormPlainTheme.load()
-// AutoForm.setDefaultTemplate('plain')
+// AutoFormPlainTheme.load()
+// // AutoForm.setDefaultTemplate('plain')
 
-new SimpleSchema({
-  name: String,
-}).validate({
-  name: 2,
-});
+// new SimpleSchema({
+//   name: String,
+// });
